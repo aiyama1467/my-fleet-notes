3c204ad4ec57abba0a77c88da63eb17454127a1d
aiyama <Aiyama2.718@gmail.com>
2025-11-15 00:59:01 +0900

    fix: standardize formatting options in Fleet component

---
+++ amplify/data/resource.ts
@@ -0,0 +1,16 @@
+import { a, type ClientSchema, defineData } from '@aws-amplify/backend';
+
+const schema = a.schema({
+  Fleet: a
+    .model({
+      title: a.string().required(),
+      tags: a.string().array(),
+      memo: a.string().required(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+export const data = defineData({
+  schema,
+});
